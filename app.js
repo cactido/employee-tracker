4851b97db9eba38fbdbc185900ea4036e2f90f00
Travis Russell <mtrussell@gmail.com>
2021-08-26 21:02:53 -0600

    added main menu prompt and start of switch for menu choice

--- app.js
+++ app.js
@@ -6,3 +6,48 @@ const connection = mysql.createConnection({
     user: 'root',
     database: 'employee_db'
 });
+
+function showAllEmployees() {
+    connection.query(`SELECT employee.id,
+        CONCAT(employee.first_name, ' ', employee.last_name) AS name, 
+        role.title, 
+        department.name AS department,
+        role.salary,  
+        CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM employee 
+        INNER JOIN role on role.id = employee.role_id 
+        INNER JOIN department on department.id = role.department_id 
+        LEFT JOIN employee m on employee.manager_id = m.id;`,
+        (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            menuPrompt();
+        });
+}
+
+function menuPrompt() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Please select an operation.',
+            name: 'choice',
+            choices: [
+                'View all employees',
+                'View all employees by department',
+                'View all employees by manager',
+                'Add new employee',
+                'Remove an employee',
+                'Update an employee\'s role',
+                'Update an employee\'s manager'
+            ]
+        }
+    ]).then((res) => {
+        switch(res.choice) {
+            case 'View all employees': showAllEmployees();
+            break;
+        }
+    })
+}
+
+menuPrompt();
+//showAllEmployees();

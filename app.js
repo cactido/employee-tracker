c3421cb4a512ef94ede292d2642db9fd99d1d084
Travis Russell <mtrussell@gmail.com>
2021-08-26 21:13:09 -0600

    added show by manager and show by department functionality

--- app.js
+++ app.js
@@ -25,6 +25,35 @@ function showAllEmployees() {
         });
 }
 
+function showByDepartment() {
+    connection.query(`SELECT employee.id,
+    CONCAT(employee.first_name, ' ', employee.last_name) AS name,
+    department.name AS department 
+    FROM employee
+    JOIN role ON employee.role_id = role.id 
+    JOIN department ON role.department_id = department.id 
+    ORDER BY department.id;`,
+    (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menuPrompt();
+    });
+}
+
+function showByManager() {
+    connection.query(`SELECT employee.id,
+    CONCAT(employee.first_name, ' ', employee.last_name) AS name,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee m on employee.manager_id = m.id
+    ORDER BY m.id;`,
+    (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        menuPrompt();
+    });
+}
+
 function menuPrompt() {
     inquirer.prompt([
         {
@@ -42,9 +71,25 @@ function menuPrompt() {
             ]
         }
     ]).then((res) => {
-        switch(res.choice) {
-            case 'View all employees': showAllEmployees();
-            break;
+        switch (res.choice) {
+            case 'View all employees':
+                showAllEmployees();
+                break;
+            case 'View all employees by department':
+                showByDepartment();
+                break;
+            case 'View all employees by manager':
+                showByManager();
+                break;
+            case 'Add new employee':
+                break;
+            case 'Remove an employee':
+                break;
+            case 'Update an employee\'s role':
+                console.log(res.choice);
+                break;
+            case 'Update an employee\'s manager':
+                break;
         }
     })
 }
